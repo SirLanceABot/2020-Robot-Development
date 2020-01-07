00575364a1fa7c3790fee071517c760b443b2555
Jon Woodard <jwoodard@lakeshoreps.org>
2020-01-07 17:32:27 -0500

    Initial project set up - jwood

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,103 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot
+{
+    /**
+     * This function is run when the robot is first started up and should be used for any initialization code.
+     */
+    @Override
+    public void robotInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically.
+     */
+    @Override
+    public void robotPeriodic()
+    {
+
+    }
+
+    /**
+     * This function is run once each time the robot enters autonomous mode.
+     */
+    @Override
+    public void autonomousInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic()
+    {
+
+    }
+
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
+    @Override
+    public void teleopInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically during teleoperated mode.
+     */
+    @Override
+    public void teleopPeriodic()
+    {
+    
+    }
+
+    /**
+     * This function is called once each time the robot enters test mode.
+     */
+    @Override
+    public void testInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic()
+    {
+
+    }
+
+        /**
+     * This function is called once each time the robot is disabled.
+     */
+    @Override
+    public void disabledInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically when the robot is disabled.
+     */
+    @Override
+    public void disabledPeriodic()
+    {
+
+    }
+}
