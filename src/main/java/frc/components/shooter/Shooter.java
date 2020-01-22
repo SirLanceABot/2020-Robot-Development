9aed9750e195e11f4f61091258178459795e4d33
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-01-22 08:12:01 -0500

    added lidar and updated shooter

--- src/main/java/frc/components/shooter/Shooter.java
+++ src/main/java/frc/components/shooter/Shooter.java
@@ -1,5 +1,6 @@
 package frc.components.shooter;
 
+import frc.sensors.LidarLite.LIDAR_Lite;
 public class Shooter 
 {
   double elevatorPosition = 0;
@@ -8,6 +9,9 @@ public class Shooter
   private static Flywheel flywheel = Flywheel.getInstance();
   private static Turret turret = Turret.getInstance();
   private static Shroud shroud = Shroud.getInstance();
+  private static LIDAR_Lite lidar = LIDAR_Lite.getInstance();
+  private static double distanceToTarget;
+  private static double angleToTarget;
   //private static Gate gate = Gate.getInstance();
 
   private static Shooter instance = new Shooter();
@@ -62,7 +66,7 @@ public class Shooter
           System.out.println("State: PreShotCheck");
         }
     },
-    ShootingOneBall() 
+    ShootingOnePowerCell() 
     {
         void doAction() 
         {
@@ -76,7 +80,7 @@ public class Shooter
           System.out.println("State: UserCorrection");
         }
     },
-    ShootingRestOfClip()
+    ShootingRestOfShuttle()
     {
         void doAction() 
         {
@@ -105,9 +109,9 @@ public class Shooter
     TrajectorySet,
     PreShotCheckPassed,
     PreShotCheckFailed,
-    FirstBallOnTarget,
-    FirstBallOffTarget,
-    ClipEmpty;
+    FirstPowerCellOnTarget,
+    FirstPowerCellOffTarget,
+    ShuttleEmpty;
   }
 
   // ----------------------------------------------------------------------//
@@ -120,96 +124,95 @@ public class Shooter
   {
     //-----------Current State --------------------------Event---------------------------NextState------//
     Transition_01(State.Off,                    Event.VisionAssistButtonPressed,        State.Searching),
-    Transition_02(State.Off,                    Event.TapeFound,                        State.Off),
-    Transition_03(State.Off,                    Event.AlignedWithTape,                  State.Off),
-    Transition_04(State.Off,                    Event.ValuesCalulated,                  State.Off),
-    Transition_05(State.Off,                    Event.TrajectorySet,                    State.Off),
-    Transition_06(State.Off,                    Event.PreShotCheckPassed,               State.Off),
-    Transition_07(State.Off,                    Event.PreShotCheckFailed,               State.Off),
-    Transition_08(State.Off,                    Event.FirstBallOnTarget,                State.Off),
-    Transition_09(State.Off,                    Event.FirstBallOffTarget,               State.Off),
-    Transition_10(State.Off,                    Event.ClipEmpty,                        State.Off),
-    Transition_11(State.Searching,              Event.VisionAssistButtonPressed,        State.Searching),
+    // Transition_02(State.Off,                    Event.TapeFound,                        State.Off),
+    // Transition_03(State.Off,                    Event.AlignedWithTape,                  State.Off),
+    // Transition_04(State.Off,                    Event.ValuesCalulated,                  State.Off),
+    // Transition_05(State.Off,                    Event.TrajectorySet,                    State.Off),
+    // Transition_06(State.Off,                    Event.PreShotCheckPassed,               State.Off),
+    // Transition_07(State.Off,                    Event.PreShotCheckFailed,               State.Off),
+    // Transition_08(State.Off,                    Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_09(State.Off,                    Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_10(State.Off,                    Event.ShuttleEmpty,                        State.Off),
+    // Transition_11(State.Searching,              Event.VisionAssistButtonPressed,        State.Off),
     Transition_12(State.Searching,              Event.TapeFound,                        State.Aligning),
-    Transition_13(State.Searching,              Event.AlignedWithTape,                  State.Aligning),
-    Transition_14(State.Searching,              Event.ValuesCalulated,                  State.Off),
-    Transition_15(State.Searching,              Event.TrajectorySet,                    State.Off),
-    Transition_16(State.Searching,              Event.PreShotCheckPassed,               State.Off),
-    Transition_17(State.Searching,              Event.PreShotCheckFailed,               State.Off),
-    Transition_18(State.Searching,              Event.FirstBallOnTarget,                State.Off),
-    Transition_19(State.Searching,              Event.FirstBallOffTarget,               State.Off),
-    Transition_20(State.Searching,              Event.ClipEmpty,                        State.Off),
-    Transition_21(State.Aligning,               Event.VisionAssistButtonPressed,        State.Aligning),
-    Transition_22(State.Aligning,               Event.TapeFound,                        State.Aligning),
-    Transition_23(State.Aligning,               Event.AlignedWithTape,                  State.Calculating),
-    Transition_24(State.Aligning,               Event.ValuesCalulated,                  State.Off),
-    Transition_25(State.Aligning,               Event.TrajectorySet,                    State.Off),
-    Transition_26(State.Aligning,               Event.PreShotCheckPassed,               State.Off),
-    Transition_27(State.Aligning,               Event.PreShotCheckFailed,               State.Off),
-    Transition_28(State.Aligning,               Event.FirstBallOnTarget,                State.Off),
-    Transition_29(State.Aligning,               Event.FirstBallOffTarget,               State.Off),
-    Transition_30(State.Aligning,               Event.ClipEmpty,                        State.Off),
-    Transition_31(State.Calculating,            Event.VisionAssistButtonPressed,        State.Calculating),
-    Transition_32(State.Calculating,            Event.TapeFound,                        State.Aligning),
-    Transition_33(State.Calculating,            Event.AlignedWithTape,                  State.Calculating),
+    // Transition_13(State.Searching,              Event.AlignedWithTape,                  State.Aligning),
+    // Transition_14(State.Searching,              Event.ValuesCalulated,                  State.Off),
+    // Transition_15(State.Searching,              Event.TrajectorySet,                    State.Off),
+    // Transition_16(State.Searching,              Event.PreShotCheckPassed,               State.Off),
+    // Transition_17(State.Searching,              Event.PreShotCheckFailed,               State.Off),
+    // Transition_18(State.Searching,              Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_19(State.Searching,              Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_20(State.Searching,              Event.ShuttleEmpty,                        State.Off),
+    // Transition_21(State.Aligning,               Event.VisionAssistButtonPressed,        State.Aligning),
+    // Transition_22(State.Aligning,               Event.TapeFound,                        State.Aligning),
+    // Transition_23(State.Aligning,               Event.AlignedWithTape,                  State.Calculating),
+    // Transition_24(State.Aligning,               Event.ValuesCalulated,                  State.Off),
+    // Transition_25(State.Aligning,               Event.TrajectorySet,                    State.Off),
+    // Transition_26(State.Aligning,               Event.PreShotCheckPassed,               State.Off),
+    // Transition_27(State.Aligning,               Event.PreShotCheckFailed,               State.Off),
+    // Transition_28(State.Aligning,               Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_29(State.Aligning,               Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_30(State.Aligning,               Event.ShuttleEmpty,                        State.Off),
+    // Transition_31(State.Calculating,            Event.VisionAssistButtonPressed,        State.Calculating),
+    // Transition_32(State.Calculating,            Event.TapeFound,                        State.Aligning),
+    // Transition_33(State.Calculating,            Event.AlignedWithTape,                  State.Calculating),
     Transition_34(State.Calculating,            Event.ValuesCalulated,                  State.SettingTrajectory),
-    Transition_35(State.Calculating,            Event.TrajectorySet,                    State.Off),
-    Transition_36(State.Calculating,            Event.PreShotCheckPassed,               State.Off),
-    Transition_37(State.Calculating,            Event.PreShotCheckFailed,               State.Off),
-    Transition_38(State.Calculating,            Event.FirstBallOnTarget,                State.Off),
-    Transition_39(State.Calculating,            Event.FirstBallOffTarget,               State.Off),
-    Transition_40(State.Calculating,            Event.ClipEmpty,                        State.Off),
-    Transition_41(State.SettingTrajectory,      Event.VisionAssistButtonPressed,        State.Searching),
-    Transition_42(State.SettingTrajectory,      Event.TapeFound,                        State.Aligning),
-    Transition_43(State.SettingTrajectory,      Event.AlignedWithTape,                  State.Calculating),
-    Transition_44(State.SettingTrajectory,      Event.ValuesCalulated,                  State.SettingTrajectory),
+    // Transition_35(State.Calculating,            Event.TrajectorySet,                    State.Off),
+    // Transition_36(State.Calculating,            Event.PreShotCheckPassed,               State.Off),
+    // Transition_37(State.Calculating,            Event.PreShotCheckFailed,               State.Off),
+    // Transition_38(State.Calculating,            Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_39(State.Calculating,            Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_40(State.Calculating,            Event.ShuttleEmpty,                        State.Off),
+    // Transition_41(State.SettingTrajectory,      Event.VisionAssistButtonPressed,        State.Searching),
+    // Transition_42(State.SettingTrajectory,      Event.TapeFound,                        State.Aligning),
+    // Transition_43(State.SettingTrajectory,      Event.AlignedWithTape,                  State.Calculating),
+    // Transition_44(State.SettingTrajectory,      Event.ValuesCalulated,                  State.SettingTrajectory),
     Transition_45(State.SettingTrajectory,      Event.TrajectorySet,                    State.PreShotCheck),
-    Transition_46(State.SettingTrajectory,      Event.PreShotCheckPassed,               State.Off),
-    Transition_47(State.SettingTrajectory,      Event.PreShotCheckFailed,               State.Off),
-    Transition_48(State.SettingTrajectory,      Event.FirstBallOnTarget,                State.Off),
-    Transition_49(State.SettingTrajectory,      Event.FirstBallOffTarget,               State.Off),
-    Transition_50(State.SettingTrajectory,      Event.ClipEmpty,                        State.Off),
-    Transition_51(State.PreShotCheck,           Event.VisionAssistButtonPressed,        State.Off),
-    Transition_52(State.PreShotCheck,           Event.TapeFound,                        State.Off),
-    Transition_53(State.PreShotCheck,           Event.AlignedWithTape,                  State.Off),
-    Transition_54(State.PreShotCheck,           Event.ValuesCalulated,                  State.Off),
-    Transition_55(State.PreShotCheck,           Event.TrajectorySet,                    State.Off),
-    Transition_56(State.PreShotCheck,           Event.PreShotCheckPassed,               State.ShootingOneBall),
+    // Transition_46(State.SettingTrajectory,      Event.PreShotCheckPassed,               State.Off),
+    // Transition_47(State.SettingTrajectory,      Event.PreShotCheckFailed,               State.Off),
+    // Transition_48(State.SettingTrajectory,      Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_49(State.SettingTrajectory,      Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_50(State.SettingTrajectory,      Event.ShuttleEmpty,                        State.Off),
+    // Transition_51(State.PreShotCheck,           Event.VisionAssistButtonPressed,        State.Off),
+    // Transition_52(State.PreShotCheck,           Event.TapeFound,                        State.Off),
+    // Transition_53(State.PreShotCheck,           Event.AlignedWithTape,                  State.Off),
+    // Transition_54(State.PreShotCheck,           Event.ValuesCalulated,                  State.Off),
+    // Transition_55(State.PreShotCheck,           Event.TrajectorySet,                    State.Off),
+    Transition_56(State.PreShotCheck,           Event.PreShotCheckPassed,               State.ShootingOnePowerCell),
     Transition_57(State.PreShotCheck,           Event.PreShotCheckFailed,               State.PreShotCheck),
-    Transition_58(State.PreShotCheck,           Event.FirstBallOnTarget,                State.Off),
-    Transition_59(State.PreShotCheck,           Event.FirstBallOffTarget,               State.Off),
-    Transition_60(State.PreShotCheck,           Event.ClipEmpty,                        State.Off),
-    Transition_61(State.ShootingOneBall,        Event.VisionAssistButtonPressed,        State.Off),
-    Transition_62(State.ShootingOneBall,        Event.TapeFound,                        State.Off),
-    Transition_63(State.ShootingOneBall,        Event.AlignedWithTape,                  State.Off),
-    Transition_64(State.ShootingOneBall,        Event.ValuesCalulated,                  State.Off),
-    Transition_65(State.ShootingOneBall,        Event.TrajectorySet,                    State.Off),
-    Transition_66(State.ShootingOneBall,        Event.PreShotCheckPassed,               State.Off),
-    Transition_67(State.ShootingOneBall,        Event.PreShotCheckFailed,               State.Off),
-    Transition_68(State.ShootingOneBall,        Event.FirstBallOnTarget,                State.ShootingRestOfClip),
-    Transition_69(State.ShootingOneBall,        Event.FirstBallOffTarget,               State.UserCorrection),
-    Transition_70(State.ShootingOneBall,        Event.VisionAssistButtonPressed,        State.Off),
-    Transition_71(State.UserCorrection,         Event.VisionAssistButtonPressed,        State.Off),
-    Transition_72(State.UserCorrection,         Event.TapeFound,                        State.Off),
-    Transition_73(State.UserCorrection,         Event.AlignedWithTape,                  State.Off),
+    // Transition_58(State.PreShotCheck,           Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_59(State.PreShotCheck,           Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_60(State.PreShotCheck,           Event.ShuttleEmpty,                        State.Off),
+    // Transition_61(State.ShootingOnePowerCell,        Event.VisionAssistButtonPressed,        State.Off),
+    // Transition_62(State.ShootingOnePowerCell,        Event.TapeFound,                        State.Off),
+    // Transition_63(State.ShootingOnePowerCell,        Event.AlignedWithTape,                  State.Off),
+    // Transition_64(State.ShootingOnePowerCell,        Event.ValuesCalulated,                  State.Off),
+    // Transition_65(State.ShootingOnePowerCell,        Event.TrajectorySet,                    State.Off),
+    // Transition_66(State.ShootingOnePowerCell,        Event.PreShotCheckPassed,               State.Off),
+    // Transition_67(State.ShootingOnePowerCell,        Event.PreShotCheckFailed,               State.Off),
+    Transition_68(State.ShootingOnePowerCell,        Event.FirstPowerCellOnTarget,                State.ShootingRestOfShuttle),
+    Transition_69(State.ShootingOnePowerCell,        Event.FirstPowerCellOffTarget,               State.UserCorrection),
+    // Transition_70(State.ShootingOnePowerCell,        Event.VisionAssistButtonPressed,        State.Off),
+    // Transition_71(State.UserCorrection,         Event.VisionAssistButtonPressed,        State.Off),
+    // Transition_72(State.UserCorrection,         Event.TapeFound,                        State.Off),
+    // Transition_73(State.UserCorrection,         Event.AlignedWithTape,                  State.Off),
     Transition_74(State.UserCorrection,         Event.ValuesCalulated,                  State.SettingTrajectory),
-    Transition_75(State.UserCorrection,         Event.TrajectorySet,                    State.Off),
-    Transition_76(State.UserCorrection,         Event.PreShotCheckPassed,               State.Off),
-    Transition_77(State.UserCorrection,         Event.PreShotCheckFailed,               State.Off),
-    Transition_78(State.UserCorrection,         Event.FirstBallOnTarget,                State.Off),
-    Transition_79(State.UserCorrection,         Event.FirstBallOffTarget,               State.Off),
-    Transition_80(State.UserCorrection,         Event.ClipEmpty,                        State.Off),
-    Transition_81(State.ShootingRestOfClip,     Event.VisionAssistButtonPressed,        State.Off),
-    Transition_82(State.ShootingRestOfClip,     Event.TapeFound,                        State.Off),
-    Transition_83(State.ShootingRestOfClip,     Event.AlignedWithTape,                  State.Off),
-    Transition_84(State.ShootingRestOfClip,     Event.ValuesCalulated,                  State.Off),
-    Transition_85(State.ShootingRestOfClip,     Event.TrajectorySet,                    State.Off),
-    Transition_86(State.ShootingRestOfClip,     Event.PreShotCheckPassed,               State.Off),
-    Transition_87(State.ShootingRestOfClip,     Event.PreShotCheckFailed,               State.Off),
-    Transition_88(State.ShootingRestOfClip,     Event.FirstBallOnTarget,                State.Off),
-    Transition_89(State.ShootingRestOfClip,     Event.ClipEmpty,                        State.Off),
-    Transition_90(State.ShootingRestOfClip,     Event.VisionAssistButtonPressed,        State.Off);
-
+    // Transition_75(State.UserCorrection,         Event.TrajectorySet,                    State.Off),
+    // Transition_76(State.UserCorrection,         Event.PreShotCheckPassed,               State.Off),
+    // Transition_77(State.UserCorrection,         Event.PreShotCheckFailed,               State.Off),
+    // Transition_78(State.UserCorrection,         Event.FirstPowerCellOnTarget,                State.Off),
+    // Transition_79(State.UserCorrection,         Event.FirstPowerCellOffTarget,               State.Off),
+    // Transition_80(State.UserCorrection,         Event.ShuttleEmpty,                        State.Off),
+    // Transition_81(State.ShootingRestOfShuttle,     Event.VisionAssistButtonPressed,        State.Off),
+    // Transition_82(State.ShootingRestOfShuttle,     Event.TapeFound,                        State.Off),
+    // Transition_83(State.ShootingRestOfShuttle,     Event.AlignedWithTape,                  State.Off),
+    // Transition_84(State.ShootingRestOfShuttle,     Event.ValuesCalulated,                  State.Off),
+    // Transition_85(State.ShootingRestOfShuttle,     Event.TrajectorySet,                    State.Off),
+    // Transition_86(State.ShootingRestOfShuttle,     Event.PreShotCheckPassed,               State.Off),
+    // Transition_87(State.ShootingRestOfShuttle,     Event.PreShotCheckFailed,               State.Off),
+    // Transition_88(State.ShootingRestOfShuttle,     Event.FirstPowerCellOnTarget,                State.Off),
+    Transition_89(State.ShootingRestOfShuttle,     Event.ShuttleEmpty,                        State.Off);
+    
     private final State currentState;
     private final Event event;
     private final State nextState;
