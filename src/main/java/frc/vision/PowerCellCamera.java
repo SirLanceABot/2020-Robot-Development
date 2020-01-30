b1fd2734c3af9bdf5531a3f97e5bb45ef9ffc411
= <=>
2020-01-29 20:44:37 -0500

    Annika - restructured vision components

---
+++ src/main/java/frc/vision/PowerCellCamera.java
@@ -0,0 +1,23 @@
+package frc.vision;
+
+/**
+ * @author Annika Price
+ */
+public class PowerCellCamera
+{
+    private static PowerCellCamera instance = new PowerCellCamera();
+
+    private PowerCellCamera()
+    {
+    }
+
+    public static PowerCellCamera getInstance()
+    {
+        return instance;
+    }
+
+    public static void detectPowerCell()
+    {
+        
+    }
+}
---
+++ src/main/java/frc/vision/RetroreflectiveTapeCamera.java
@@ -0,0 +1,23 @@
+package frc.vision;
+
+/**
+ * @author Annika Price
+ */
+public class RetroreflectiveTapeCamera
+{
+    private static RetroreflectiveTapeCamera instance = new RetroreflectiveTapeCamera();
+
+    private RetroreflectiveTapeCamera()
+    {
+    }
+
+    public static RetroreflectiveTapeCamera getInstance()
+    {
+        return instance;
+    }
+
+    public static void detectHighGoal()
+    {
+        
+    }
+}
