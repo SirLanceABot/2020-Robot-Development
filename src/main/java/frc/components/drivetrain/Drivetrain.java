2008e9d2ea7ff79e9057614833138fae8f0cacab
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-02-08 15:02:17 -0500

    added methods to drivetrain for auto code

--- src/main/java/frc/components/drivetrain/Drivetrain.java
+++ src/main/java/frc/components/drivetrain/Drivetrain.java
@@ -3,6 +3,7 @@ package frc.components.drivetrain;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 public class Drivetrain extends DifferentialDrive
 {
@@ -10,7 +11,7 @@ public class Drivetrain extends DifferentialDrive
     private static WPI_TalonSRX backRightMotor = new WPI_TalonSRX(2);
     private static WPI_TalonSRX backLeftMotor = new WPI_TalonSRX(3);
     private static WPI_TalonSRX frontLeftMotor = new WPI_TalonSRX(4);
-    
+
     private static SpeedControllerGroup rightMotors = new SpeedControllerGroup(frontRightMotor, backRightMotor);
     private static SpeedControllerGroup leftMotors = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
 
@@ -29,6 +30,13 @@ public class Drivetrain extends DifferentialDrive
         frontLeftMotor.setInverted(false);
         backRightMotor.setInverted(true);
         backLeftMotor.setInverted(false);
+
+		frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
+		frontLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
+		backRightMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
+		backLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
+		
+		resetEncoders();
 
         /**
          * This one inverts within software (flips a boolean)
@@ -62,5 +70,42 @@ public class Drivetrain extends DifferentialDrive
     public void rotate(int rotation)
     {
 
+    }
+
+    public void resetEncoders()
+    {
+        frontRightMotor.setSelectedSensorPosition(0);
+        frontLeftMotor.setSelectedSensorPosition(0);
+        backRightMotor.setSelectedSensorPosition(0);
+        backLeftMotor.setSelectedSensorPosition(0);
+    }
+
+    public void stop()
+    {
+        westCoastDrive(0, 0);
+    }
+
+    public double getLeftPosition()
+    {
+        return (frontLeftMotor.getSelectedSensorPosition(0)
+        + backLeftMotor.getSelectedSensorPosition(0)) / 2.0;
+    }
+
+    public double getRightPosition()
+    {
+        return (frontRightMotor.getSelectedSensorPosition(0)
+        + backRightMotor.getSelectedSensorPosition(0)) / 2.0;
+    }
+
+    public void setLeftPower(double power)
+    {
+        frontLeftMotor.set(power);
+        backLeftMotor.set(power);
+    }
+
+    public void setRightPower(double power)
+    {
+        frontRightMotor.set(power);
+        backRightMotor.set(power);
     }
 }
