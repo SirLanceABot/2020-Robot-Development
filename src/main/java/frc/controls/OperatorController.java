0d1b84e74132d36ccab6b1b69f0f66784651f176
unknown <lakeshoreroboticsteam@gmail.com>
2020-02-12 20:57:32 -0500

    added getAction things - ishaan

--- src/main/java/frc/controls/OperatorController.java
+++ src/main/java/frc/controls/OperatorController.java
@@ -2,6 +2,31 @@ package frc.controls;
 
 public class OperatorController extends Logitech
 {
+    public enum ButtonAction
+    {
+        kShoot(Button.kTrigger);
+
+        public final Button button;
+
+        private ButtonAction(Button button)
+        {
+           this.button = button;
+        }
+    }
+
+    public enum AxisAction
+    {
+        kMoveTurret(Axis.kZAxis),
+        kMoveShroud(Axis.kYAxis);
+
+        public final Axis axis;
+
+        private AxisAction(Axis axis)
+        {
+           this.axis = axis;
+        }
+    }
+
     private static final int OPERATOR_CONTROLLER_PORT = 1;
 
     private static OperatorController instance = new OperatorController(OPERATOR_CONTROLLER_PORT);
@@ -22,5 +47,27 @@ public class OperatorController extends Logitech
     public static OperatorController getInstance()
     {
         return instance;
+    }
+
+    @Deprecated
+    public boolean getRawButton(Button button)
+    {
+        return super.getRawButton(button);
+    }
+
+    @Deprecated
+    public double getRawAxis(Axis axis)
+    {
+        return super.getRawAxis(axis);
+    }
+
+    public boolean getAction(ButtonAction buttonAction)
+    {
+        return getRawButton(buttonAction.button);
+    }
+
+    public double getAction(AxisAction axisAction)
+    {
+        return getRawAxis(axisAction.axis);
     }
 }
