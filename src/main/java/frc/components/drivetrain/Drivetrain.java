feec0c774373a844f21e1e82b8d9c40a089398f2
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-02-18 20:56:30 -0500

    added methods to drive and rotate

--- src/main/java/frc/components/drivetrain/Drivetrain.java
+++ src/main/java/frc/components/drivetrain/Drivetrain.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import frc.sensors.NavX;
+import frc.shuffleboard.AutonomousTab.StartingLocation;
 
 public class Drivetrain extends DifferentialDrive
 {
@@ -25,6 +27,20 @@ public class Drivetrain extends DifferentialDrive
     private static SpeedControllerGroup rightMotors = new SpeedControllerGroup(frontRightMotor, backRightMotor);
     private static SpeedControllerGroup leftMotors = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
 
+    private static NavX navX = NavX.getInstance();
+
+    private static boolean driveInitFlag = true;
+    private static double targetDistance = 0.0;
+    private static double ticksPerRotation = 4096; //TODO: need to find real value
+    private static double diameterOfWheel = 6; // in inches;
+    private static double distanceToTravel = 0.0;
+    private static double startingEncoderValue = 0.0;
+    private static double distanceTraveled = 0.0;
+    private static boolean movingForward = false;
+    private static double percentMax = 0.75;
+
+    private static boolean rotationInitFlag = true;
+    private static double targetRotation = 0.0;
     private static Drivetrain instance = new Drivetrain();
 
     private Drivetrain()
@@ -78,14 +94,126 @@ public class Drivetrain extends DifferentialDrive
         super.arcadeDrive(move, rotate, squared);
     }
 
-    public void move(int distance)
+    private double inchesToTicks(double inches)
     {
+        return (inches/(diameterOfWheel * Math.PI) * ticksPerRotation);
+    }
+    /**
+     * Drives a distance forward
+     * IN INCHES
+     * @param distance
+     */
+    public boolean drive(double distance)
+    {
+        if(driveInitFlag)
+        {
+            driveInitFlag = false;
+            startingEncoderValue = (getLeftPosition() + getRightPosition()) / 2.0;
+            distanceToTravel = inchesToTicks(distance) -  startingEncoderValue;
+            targetDistance = inchesToTicks(distance) + startingEncoderValue;
+            if(targetDistance < 0)
+            {
+                movingForward = false;
+            }
+            else
+            {
+                movingForward = true;
+            }
+        }
 
+        distanceTraveled = ((getLeftPosition() + getRightPosition()) / 2.0) - startingEncoderValue;
+        
+        if(Math.abs(distanceTraveled) > targetDistance - 5 && Math.abs(distanceTraveled) < targetDistance + 5)
+        {
+            driveInitFlag = true;
+            westCoastDrive(0.0, 0.0);
+            return true;
+        }
+        else
+        {   
+            if(Math.abs(distanceTraveled/distanceToTravel) > 0.1)
+            {
+                if(movingForward)
+                {
+                    westCoastDrive(distanceTraveled/distanceToTravel * percentMax, 0);
+                    
+                }
+                else
+                {
+                    westCoastDrive(-distanceTraveled/distanceToTravel * percentMax, 0);
+    
+                }
+            }
+            else
+            {
+                if(movingForward)
+                {
+                    westCoastDrive(0.1, 0);
+                }
+                else
+                {
+                    westCoastDrive(-0.1, 0);
+                }
+            }
+            return false;
+        }
     }
 
-    public void rotate(int rotation)
-    {
+        
 
+
+    /**
+     * Relative rotation, NOT ABSOLUTE
+     * @param rotation
+     */
+    public void rotate(double rotation)
+    {
+        
+        double currentAngle = navX.getAngle();
+        double targetAngle = rotation + currentAngle;
+        if(targetAngle < -1.0)
+        {   
+            westCoastDrive(0.0, -0.25);
+        }
+        else if(targetAngle > 1.0)
+        {
+            westCoastDrive(0.0, 0.25);
+        }
+        else
+        {
+            westCoastDrive(0.0, 0.0);
+        }
+    }
+
+    /**
+     * Absolute Rotation, ABSOLUTE
+     * @param rotation
+     */
+    public boolean rotateTo(double rotation)
+    {
+        if(rotationInitFlag)
+        {
+            targetRotation = rotation; 
+            rotationInitFlag = false;
+            return false;
+        }
+        double currentAngle = navX.getAngle();
+
+        if(targetRotation - currentAngle  < -1.0)
+        {   
+            westCoastDrive(0.0, -0.25);
+            return false;
+        }
+        else if(targetRotation - rotation > 1.0)
+        {
+            westCoastDrive(0.0, 0.25);
+            return false;
+        }
+        else
+        {
+            westCoastDrive(0.0, 0.0);
+            return true;
+        }    
     }
 
     public void resetEncoders()
