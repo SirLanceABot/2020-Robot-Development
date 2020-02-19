d011e4bb3f5114de8131ac8c3d5efee48073676f
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-02-19 08:49:14 -0500

    added comments to rotate

--- src/main/java/frc/autonomous/commands/Rotate.java
+++ src/main/java/frc/autonomous/commands/Rotate.java
@@ -3,22 +3,32 @@ package frc.autonomous.commands;
 import frc.autonomous.commands.interfaces.*;
 import frc.components.drivetrain.Drivetrain;
 
+/**
+ * Autonomous command to rotate to an absolute angle
+ * @author Maxwell Li
+ */
 public class Rotate implements Command
 {
+    //Init the instance variables
     private static Drivetrain drivetrain = Drivetrain.getInstance();
     private static double absoluteAngle;
     private static boolean isFinished;
 
+    //construct the object
     public Rotate(double angle)
     {
         absoluteAngle = angle;
     }
 
+    //init function to be called first when executing commands
+    @Override
     public void init()
     {
         System.out.println("Initializing Rotation");
     }
 
+    //function to be called after init to execute 
+    @Override
     public void execute()
     {   
         System.out.println("Executing Rotation");
@@ -30,17 +40,17 @@ public class Rotate implements Command
         {
             isFinished = false;
         }
-        // if(drivetrain.stop());
-        // {
-        //     isFinished = true;
-        // }
     }
 
+    //getter funtion for the the isFinished flag
+    @Override
     public boolean isFinished()
     {
         return isFinished;
     }
 
+    //function that ends the command
+    @Override
     public void end()
     {
         System.out.println("Ending Rotation");
