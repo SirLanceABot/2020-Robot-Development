b436aa615cc3fe57de08a404904a8d8f8f79094a
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-02-19 08:56:13 -0500

    added comments

--- src/main/java/frc/autonomous/commands/Drive.java
+++ src/main/java/frc/autonomous/commands/Drive.java
@@ -3,22 +3,33 @@ package frc.autonomous.commands;
 import frc.autonomous.commands.interfaces.*;
 import frc.components.drivetrain.Drivetrain;
 
+/**
+ * Auotonomous command to drive a certain relative distance
+ * (only forward and backward)
+ * @author Maxwell Li
+ */
 public class Drive implements Command
 {
+    //creation/init of instance variables
     private static Drivetrain drivetrain = Drivetrain.getInstance();
     private static boolean isFinished;
     private static double distance;
 
+    //constuctor (takes the distance to drive)
     public Drive(double distanceToDrive)
     {
         distance = distanceToDrive;
     }
 
+    //initialization function, first called when executing the command
+    @Override
     public void init()
     {
         System.out.println("Initializing Drive");
     }
 
+    //execution function that is called when executing the command
+    @Override
     public void execute()
     {   
         System.out.println("Executing Drive");
@@ -29,11 +40,15 @@ public class Drive implements Command
         }
     }
 
+    //getter function for the isFinished flag
+    @Override
     public boolean isFinished()
     {
         return isFinished;
     }
 
+    //function to be called when the command ends
+    @Override
     public void end()
     {
         System.out.println("Ending Drive");
