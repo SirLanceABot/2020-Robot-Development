0c361385b48a72056d3393e7ab7c40664032f77b
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-02-18 20:56:03 -0500

    added absolute drive to command

---
+++ src/main/java/frc/autonomous/commands/DriveTo.java
@@ -0,0 +1,46 @@
+package frc.autonomous.commands;
+
+import javax.lang.model.util.ElementScanner6;
+
+import frc.autonomous.commands.interfaces.*;
+import frc.components.drivetrain.Drivetrain;
+import frc.components.powercellsupervisor.shooter.Shooter;
+
+public class DriveTo implements Command
+{
+    private static Drivetrain drivetrain = Drivetrain.getInstance();
+    private static double absoluteAngle;
+    private static boolean isFinished;
+    private static double xPos = 0.0;
+    private static double yPos = 0.0;
+
+    public DriveTo(double xPosition, double yPosition)
+    {
+        xPos = xPosition;
+        yPos = yPosition;
+    }
+
+    public void init()
+    {
+        System.out.println("Initializing Drive To:" + xPos + "," + yPos);
+    }
+
+    public void execute()
+    {   
+        System.out.println("Executing Drive To: " + xPos + "," + yPos);
+        //Need to add the math for an absolute coordiante system
+        //will combine the drive and the rotate class,
+        //merely acts as a wrapper for the two and coordinates them
+    }
+
+
+    public boolean isFinished()
+    {
+        return isFinished;
+    }
+
+    public void end()
+    {
+        System.out.println("Ending Drive" + xPos + "," + yPos);
+    }
+}
