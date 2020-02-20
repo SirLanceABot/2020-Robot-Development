b195de55c2ef288a356328e809a0a7b53232ce63
unknown <lakeshoreroboticsteam@gmail.com>
2020-02-19 20:16:01 -0500

    slider can only be 0-1 now - ig

--- src/main/java/frc/controls/Logitech.java
+++ src/main/java/frc/controls/Logitech.java
@@ -95,6 +95,10 @@ public class Logitech extends Joystick
     {
         double value = super.getRawAxis(axis);
 
+        if(axis == Axis.kSlider.value)
+        {
+            value = (value + 1.0) / 2.0;  // scales slider so it goes from 0 to 1 instead of -1 to 1
+        }
         if(axisIsFlipped[axis])
         {
             value *= -1;
