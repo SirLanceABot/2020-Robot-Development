02820b07f956cd70da9c38c44ca5b823c3e1682e
Jon Woodard <jwoodard@lakeshoreps.org>
2020-03-11 08:08:39 -0400

    added axis settings to AxisAction - jw

--- src/main/java/frc/controls/OperatorController.java
+++ src/main/java/frc/controls/OperatorController.java
@@ -16,16 +16,20 @@ public class OperatorController extends Logitech
     {
         kShoot(Button.kTrigger),
         kAutoAim(Button.kHandleSide),
-        kTurretOverride(Button.kHandleBottomLeft),
-        // kShooterOverride(Button.kInnerTop),
-        // kShuttleOverride(Button.kOuterTop),
+
+        kOffTarget(Button.kHandleBottomLeft),
+        kOnTarget(Button.kHandleBottomRight),
+        // kNoAction(Button.kHandleTopLeft),
+        // kNoAction(Button.kHandleTopRight),
+
+        // kNoAction(Button.kOuterTop),
+        // kNoAction(Button.kInnerTop),
         kFlywheelOverride(Button.kOuterMiddle),
         kShooterOverride(Button.kInnerMiddle),
-        kShuttleOverride(Button.kInnerBottom),
         kWinch(Button.kOuterBottom),
-        kOnTarget(Button.kHandleBottomRight),
-        kOffTarget(Button.kHandleBottomLeft);
+        kShuttleOverride(Button.kInnerBottom) 
         ;
+
         public final Button button;
 
         private OperatorButtonAction(Button button)
@@ -36,16 +40,26 @@ public class OperatorController extends Logitech
 
     public enum OperatorAxisAction
     {
-        kTurret(Axis.kZAxis),
-        kShroud(Axis.kYAxis),
-        //kShuttle(Axis.kYAxis),
-        kShooterPower(Axis.kSlider);
+        // kNoAction(Axis.kXAxis, 0.1, 0.0, 1.0, false, AxisScale.kLinear),
+        kShroud(Axis.kYAxis, 0.2, 0.0, 1.0, true, AxisScale.kLinear),
+        kTurret(Axis.kZAxis, 0.25, 0.0, 1.0, false, AxisScale.kLinear),
+        kShooterPower(Axis.kSlider, 0.1, 0.0, 1.0, true, AxisScale.kLinear);
 
         public final Axis axis;
+        public final double axisDeadzone;
+        public final double axisMinOutput;
+        public final double axisMaxOutput;
+        public final boolean axisIsFlipped;
+        public final AxisScale axisScale;
 
-        private OperatorAxisAction(Axis axis)
+        private OperatorAxisAction(Axis axis, double axisDeadzone, double axisMinOutput, double axisMaxOutput, boolean axisIsFlipped, AxisScale axisScale)
         {
-           this.axis = axis;
+            this.axis = axis;
+            this.axisDeadzone = axisDeadzone;
+            this.axisMinOutput = axisMinOutput;
+            this.axisMaxOutput = axisMaxOutput;
+            this.axisIsFlipped = axisIsFlipped;
+            this.axisScale = axisScale;
         }
     }
 
@@ -57,10 +71,10 @@ public class OperatorController extends Logitech
 
         System.out.println(className + " : Constructor Started");
 
-        setAxisSettings(Axis.kXAxis, 0.1, 0.0, 1.0, false, AxisScale.kLinear);
-        setAxisSettings(Axis.kYAxis, 0.2, 0.0, 1.0, true, AxisScale.kLinear);
-        setAxisSettings(Axis.kZAxis, 0.25, 0.0, 1.0, false, AxisScale.kLinear);
-        setAxisSettings(Axis.kSlider, 0.1, 0.0, 1.0, true, AxisScale.kLinear);
+        for(OperatorAxisAction action : OperatorAxisAction.values())
+        {
+            setAxisSettings(action.axis, action.axisDeadzone, action.axisMinOutput, action.axisMaxOutput, action.axisIsFlipped, action.axisScale);
+        }
 
         System.out.println(className + ": Constructor Finished");
     }
@@ -77,7 +91,19 @@ public class OperatorController extends Logitech
     }
 
     @Deprecated
+    public boolean getRawButton(int button)
+    {
+        return super.getRawButton(button);
+    }
+
+    @Deprecated
     public double getRawAxis(Axis axis)
+    {
+        return super.getRawAxis(axis);
+    }
+
+    @Deprecated
+    public double getRawAxis(int axis)
     {
         return super.getRawAxis(axis);
     }
