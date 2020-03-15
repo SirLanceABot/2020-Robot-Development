c82e8b45c8c57a6cabd50994343632818878d1b1
TheDragonCodeMaster <49327666+TheDragonCodeMaster@users.noreply.github.com>
2020-03-15 02:04:10 -0400

    added gyro to snap to power port

--- src/main/java/frc/components/powercellsupervisor/shooter/Shooter.java
+++ src/main/java/frc/components/powercellsupervisor/shooter/Shooter.java
@@ -68,7 +68,7 @@ public class Shooter implements Notified
       {
         turnLightOn();
         System.out.println("State: Searching");
-        //turret.rotateToWall();
+        turret.rotateToWall();
         System.out.println(turretVision.isTargetFound() + "\tframe number: " + turretVision.getFrameNumber());
         if(turretVision.isFreshData())
         {
